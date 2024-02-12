379f19d4adcd26ced3e9dbd381c8d43590c0ced8
Samuel <samuel@devsquad.com>
2024-02-12 10:07:21 -0300

    add components and implement the design system

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
+
 import Groups from "@screens/Groups";
 
 export default function App() {
-  return (
-      <Groups />
-  );
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            { fontsLoaded ? <Groups /> : <Loading /> }
+        </ThemeProvider>
+    );
 }
